ca58cc7ac4e1ae03689c5aa6992abfb86baa4112
Melissa Hill <mhill2040@gmail.com>
2018-02-12 15:58:29 -0700

    wip

--- slots.rb
+++ slots.rb
@@ -1,18 +1,39 @@
 class Slots
-  attr_reader :player, :casino
-
-  def initialize(player, casino)
-    @player = player
-    @casino = casino
+  def initialize
+    puts "Welcome to my slot machine"
+    place_bet
   end
 
-  def play_game
-    puts '1) spin'
-    puts '2) exit to casino'
-    choice = gets.to_i
-    if choice == 2
-      @casino.menu
-    end
+  def place_bet
+    puts "Type in the amount you would like to bet."
+    bet = gets.to_i
+    run_slots
   end
+
+def run_slots
+  puts "==============="
+  puts "Match all 3 to win"
+  option1 = rand(1..9)
+  option2 = rand(1..9)
+  option3 = rand(1..9)
+  puts option1
+  puts option2
+  puts option3
+
+  if option1 == option2 && option2 == option3
+    puts "You Win"
+  else
+    puts "You lose"
+  end
+end
+
+
 
 end
+
+Slots.new
+
+
+#elseif match 2 you win half the bet
+#keep it playing
+#exit
