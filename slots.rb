2c281e8ce4d37882374c6a193e35fe43e68a3c02
Melissa Hill <mhill2040@gmail.com>
2018-02-12 22:01:06 -0700

    partial wins

--- slots.rb
+++ slots.rb
@@ -1,18 +1,18 @@
 class Slots
   def initialize
-    puts "Welcome to my slot machine"
+    puts "Welcome to my slot machine."
     place_bet
   end
 
   def place_bet
-    puts "Type in the amount you would like to bet."
-    bet = gets.to_i
+    puts "Please type in the dollar amount you would like to bet."
+    @bet = gets.to_i
     run_slots
   end
 
 def run_slots
-  puts "==============="
-  puts "Match all 3 to win"
+  puts "==========================="
+  puts "Match numbers to win"
   option1 = rand(1..9)
   option2 = rand(1..9)
   option3 = rand(1..9)
@@ -22,18 +22,31 @@ def run_slots
 
   if option1 == option2 && option2 == option3
     puts "You Win"
+    #TODO add @bet * 2 to wallet
+  elsif option1 == option2
+    puts "You Win"
+    #TODO add @bet / 2 to wallet
+  elsif option2 == option3
+    puts "You Win"
+    #TODO add @bet / 2 to wallet
+  elsif option1 == option3
+    puts "You Win"
+    #TODO add @bet / 2 to wallet
   else
     puts "You lose"
   end
+
+  #ask to play again or exit
+  puts "Do you want to play again y / n"
+  answer = gets.strip.downcase
+  if awswer == 'y'
+    place_bet
+  else
+    #TODO exit to main Casino
+  end
+    
 end
-
-
 
 end
 
 Slots.new
-
-
-#elseif match 2 you win half the bet
-#keep it playing
-#exit
