0ed9d24683f390a4aa1636143111c32a4037bae6
Ian Harwell <sirharwell@gmail.com>
2018-02-12 23:28:57 -0700

    fix player

--- player.rb
+++ player.rb
@@ -1,3 +1,19 @@
 class Player
+  attr_accessor :name, :wallet
+
+  def initialize(name, wallet)
+    @name = name
+    @wallet = wallet
+  end
+
+def self.add(amount)
+  @wallet += amount
+  puts "You have $#{@wallet} in your wallet."
+end
+
+def self.subtract(amount)
+  @awallet -= amount
+  puts "You have $#{@wallet} in your wallet."
+end
 
 end
